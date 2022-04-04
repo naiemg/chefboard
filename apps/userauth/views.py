e83f17272899e86cefb8ea03633b8192409f2abf
naiemg <naiem.gafar@gmail.com>
2022-04-04 16:52:13 -0400

    updated dev notes for stripe cli

--- apps/userauth/views.py
+++ apps/userauth/views.py
@@ -146,7 +146,7 @@ def stripe_config(request):
 @csrf_exempt
 def create_checkout_session(request):
     if request.method == 'GET':
-        domain_url = 'http://localhost:8000/'
+        domain_url = 'http://192.168.1.3:8000/'
         stripe.api_key = settings.STRIPE_SECRET_KEY
         try:
             checkout_session = stripe.checkout.Session.create(
